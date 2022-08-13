1f4eb57b60a9fea32728ef5f211a2f8db6fd2f3d
Aksyon <aksenovaleksander94@gmail.com>
2022-08-13 17:43:50 +0500

    add the get_week_sats code and converting currency

--- CalculatorLogic.py
+++ CalculatorLogic.py
@@ -1,8 +1,22 @@
 import datetime as dt
 
-dt.datetime.today()
+today = dt.datetime.today().date()
+day_week_ago = today - dt.timedelta(days = 6)
 
-class Calculator:
+class Records:
+
+    def __init__(self, amount, comment, date):
+        
+        self.amount = amount
+        self.comment = comment
+                
+        if date == None:
+            date = dt.datetime.today()
+        else:
+            date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+        self.date = date
+        
+class Calculator():
     
     def __init__(self, limit):
        
@@ -13,46 +27,53 @@ class Calculator:
         
     def add_record(self, records):
         
-        self.records.append(records)        
-        return records
+       self.records.append(records)        
+       return records
 
-    def get_today_stats(self):
+    def get_today_stats():
         
-        if self.date == dt.datetime.today():
-            dayStats = dayStats + self.amount
-        return dayStats
+        date = Records.date
+       
+        if date == dt.datetime.today():
+            dayStats = dayStats + amount
+            return dayStats
                 
     def get_week_stats():
-        pass
+       
+        if day_week_ago < day < today:
+            weekStats = weekStats + amount
+            return weekStats 
      
-class Records(Calculator):
-
-    def __init__(self, amount, comment, date):
-        self.amount = amount
-        self.comment = comment
-        self.date = date
-
-        if date == None:
-            self.date = dt.datetime.today()
-        
-        self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
-
 class CashCalculator(Calculator):
     
+    def __init__(self, limit, usd_rate = 62.25, euro_rate = 63.88):
+        
+        Calculator.__init__(self, limit)
+        self.usd_rate = usd_rate
+        self.euro_rate = euro_rate
+        
     def get_today_cash_remained(currency):
         
         N = limit - dayStats
+
+        if currency == 'USD':
+            N = N * usd_rate
+        elif currency == 'EURO':
+            N = N * euro_rate
+
         if dayStats < limit:
-            return f'На сегодня осталось {N} {currency} :)'
+            return f'На сегодня осталось {N} рублей :)'
         elif dayStats == limit:
             return 'Денег нет, держись :/'
         else:
-            return f'Денег нет, держись. Твой долг {N} {currency}'
-
+            return f'Денег нет, держись. Твой долг {N} рублей'
 
 class CaloriesCalculator(Calculator):
-    
+        
     def get_calories_remained():
+        
+        dayStats = Calculator.get_today_stats
+        limit = Calculator.limit
         N = limit - dayStats
         if dayStats < limit:
             return f'Сегодня можно съесть еще что-нибудь, но с общей каллорийностью не более {N} кКал :)'
@@ -61,10 +82,10 @@ class CaloriesCalculator(Calculator):
    
 cash = CashCalculator(5000)
 
-cash.add_record(Records(amount = 1500, comment = 'кафе', date = '08.03.22'))
+cash.add_record(Records(amount = 1500, comment = 'кафе', date = '08.03.2022'))
 
-cash.add_record(Records(amount = 500, comment = 'продукты', date = '08.03.22'))
+cash.add_record(Records(amount = 500, comment = 'продукты', date = '08.03.2022'))
 
-cash.add_record(Records(amount = 2000, comment = 'йога', date = '08.03.22'))
+cash.add_record(Records(amount = 2000, comment = 'йога', date = '08.03.2022'))
 
-print(cash.get_today_stats())
+print(Calculator.get_today_stats())
