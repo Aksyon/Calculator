04a85fbc39b66fdfafc5f1062eaa27e5df38a9b1
Aksyon <aksenovaleksander94@gmail.com>
2022-08-15 09:57:14 +0500

    Optimised cod to PEP 8

--- CalculatorLogic.py
+++ CalculatorLogic.py
@@ -1,15 +1,14 @@
 import datetime as dt
 
 today = dt.datetime.today().date()
-day_week_ago = today - dt.timedelta(days = 6)
+day_week_ago = today - dt.timedelta(days=6)
 
 class Records:
-
+    
+    
     def __init__(self, amount, comment, date):
-        
         self.amount = amount
-        self.comment = comment
-                
+        self.comment = comment      
         if date == None:
             self.date = dt.datetime.today().date()
         else:
@@ -18,15 +17,14 @@ class Records:
         
 class Calculator():
     
+    
     def __init__(self, limit):
-       
         self.limit = limit
         self.records = []
         self.dayStats = []
         self.weekStats = []
         
     def add_record(self, record):
-        
        self.records.append(record)        
        return self.records
 
@@ -44,24 +42,21 @@ class Calculator():
      
 class CashCalculator(Calculator):
     
-    def __init__(self, limit):
-        
-        Calculator.__init__(self, limit)
-        self.usd_rate = 62.25
-        self.euro_rate = 63.88
+
+    usd_rate = 62.25
+    euro_rate = 63.88
                 
     def get_today_cash_remained(self, currency):
         limit = self.limit
-        dayStatsSum = Calculator.get_today_stats(self)     # WTF? become 2 times more
-        print(dayStatsSum)
-        dif = limit - dayStatsSum
-        
-        if currency == 'USD':              #converting currency to 'RUB'
-            N = dif * self.usd_rate
+        dayStatsSum = self.get_today_stats()  
+        difference = limit - dayStatsSum
+       
+        if currency == 'USD': #converting currency to 'RUB'
+            N = difference * self.usd_rate
         elif currency == 'EURO':
-            N = dif * self.euro_rate
+            N = difference * self.euro_rate
         elif currency == 'RUB':
-            N = dif
+            N = difference
         
         if dayStatsSum < limit:
             return f'На сегодня осталось {N} рублей :)'
@@ -71,24 +66,34 @@ class CashCalculator(Calculator):
             return f'Денег нет, но вы держитесь. Долг {N} рублей'
         
 class CaloriesCalculator(Calculator):
+
+
+    def get_calories_remained(self):
         
-    def get_calories_remained():
-        
-        dayStats = Calculator.get_today_stats()
-        limit = Calculator.limit
+        dayStats = self.get_today_stats()
+        limit = self.limit
         N = limit - dayStats
+        
         if dayStats < limit:
-            return f'Сегодня можно съесть еще что-нибудь, но с общей каллорийностью не более {N} кКал :)'
+            return (f'Сегодня можно съесть еще что-нибудь,'
+                   f'но с общей каллорийностью не более {N} кКал :)')
         else:
             return 'Хватит есть! :/'
-   
-cash = CashCalculator(1000)
 
-cash.add_record(Records(amount = 250, comment = 'кафе', date = '14.08.2022'))
+if __name__ == '__main__':
 
-#cash.add_record(Records(amount = 50, comment = 'продукты', date = '14.08.2022'))
+    cash = CashCalculator(500)
 
-#cash.add_record(Records(amount = 50, comment = 'йога', date = '14.08.2022'))
+    cash.add_record(Records(amount = 250,
+                           comment = 'кафе',
+                           date = '14.08.2022'))
 
-print(Calculator.get_today_stats(cash))
-print(cash.get_today_cash_remained('RUB'))
+    cash.add_record(Records(amount = 50,
+                           comment = 'продукты',
+                           date = '14.08.2022'))
+
+    cash.add_record(Records(amount = 200,
+                           comment = 'йога',
+                           date = '14.08.2022'))
+
+    print(cash.get_today_cash_remained('USD'))
