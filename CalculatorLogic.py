260dacc790fdd2707d4c5081262a279968bf86a9
Aksyon <aksenovaleksander94@gmail.com>
2022-08-23 20:51:46 +0500

    Fixed remarks

--- CalculatorLogic.py
+++ CalculatorLogic.py
@@ -2,107 +2,111 @@
 of cash remained and also calories remained """
 
 import datetime as dt
-
-today = dt.datetime.today().date()
-day_week_ago = today - dt.timedelta(days=6)
+FORMAT = '%d.%m.%Y'
 
 
-class Records:
+class Records():
     """Class for saving the records"""
     
     def __init__(self, amount, comment, date):
-        self.amount = amount
-        self.comment = comment      
-        if date == None:
+        self.amount = float(amount)
+        self.comment = str(comment)     
+        if date is None:
             self.date = dt.datetime.today().date()
         else:
-            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+            self.date = dt.datetime.strptime(date, FORMAT).date()
         
 
 class Calculator():
     """Class with main logic of calculation"""
     
     def __init__(self, limit):
-        self.limit = limit
+        self.limit = int(limit)
         self.records = []
         self.dayStats = []
-        self.weekStats = []
-        
+        self.ad = 0
+
     def add_record(self, record):
         """Function for addiing the records in list"""
         self.records.append(record)        
         return self.records
 
     def get_today_stats(self):
-        """Function for getting the statistics for the whole that day"""
-        for record in self.records:
-            if record.date == today:
-                self.dayStats.append(record.amount)
-        return sum(self.dayStats)
-                
+        """Function for getting the statistics for the whole this day"""
+        composeDayStats = [amount for amount in self.records if amount.date ==
+                          dt.datetime.today().date()]
+        return sum(composeDayStats)
+
     def get_week_stats(self):
         """Function for getting the statistics for previous week"""
-        for record in self.records:
-            if day_week_ago <= record.date < today:
-                self.weekStats.append(record.amount)
-        return sum(self.weekStats)
-     
-     
+        today = dt.datetime.today().date()
+        day_week_ago = today - dt.timedelta(days=6)
+        composeWeekStats = [amount for amount in self.records if day_week_ago 
+                           <= amount.date < today]
+        return sum(composeWeekStats)
+
+    def getDifference(self):
+        getDaySum = self.get_today_stats()
+        isDifference = self.limit - getDaySum
+        return isDifference
+    
+
 class CashCalculator(Calculator):
     """Class with functions for cash statistics"""
-
-    usd_rate = 62.25
-    euro_rate = 63.88
-                
+    CURRENCY = {'USD_RUB' : [62.25, 'в рублях'],
+                'EURO_RUB' : [63.88, 'в рублях'],
+                'USD_EURO' : [1, 'в евро'],
+                'EURO_USD' : [1, 'в долларах'],
+                'RUB' : [1, 'в рублях']
+    }
+    CASH_BALANCE_PLUS = 'На сегодня осталось {currency_name}: {balance}'
+    CASH_BALANCE_MINUS = ('Денег нет, но вы держитесь. Долг {currency_name}: '
+                         '{balance}')
+    CASH_BALANCE_ZERO = 'Денег нет, но вы держитесь :/'
+    
     def get_today_cash_remained(self, currency):
         """Function for calculation of your cash balance at this moment"""
-        limit = self.limit
-        dayStatsSum = self.get_today_stats()  
-        difference = limit - dayStatsSum
-       
-        if currency == 'USD': #converting currency to 'RUB'
-            N = difference * self.usd_rate
-        elif currency == 'EURO':
-            N = difference * self.euro_rate
-        elif currency == 'RUB':
-            N = difference
-        
-        if dayStatsSum < limit:
-            return f'На сегодня осталось {N} рублей :)'
-        elif dayStatsSum == limit:
-            return 'Денег нет, но вы держитесь :/'
-        else:
-            return f'Денег нет, но вы держитесь. Долг {N} рублей'
+        isDifference = self.getDifference()
+        getRate = self.CURRENCY[currency]
+        getCashRemained = isDifference * getRate[0]
+      
+        if getCashRemained != 0:
+            if getCashRemained > 0:
+                return self.CASH_BALANCE_PLUS.format(
+                    currency_name = getRate[1], balance=getCashRemained)
+            return self.CASH_BALANCE_MINUS.format(
+                currency_name = getRate[1], balance=getCashRemained)
+        if getCashRemained == 0:
+            return self.CASH_BALANCE_ZERO
+
         
 class CaloriesCalculator(Calculator):
     """Class with functions for calories statistics"""
+    CALORIES_BALANCE = ('Сегодня можно съесть еще что-нибудь, но с общей'
+                       'каллорийностью не более {balance} кКал :)')
+    STOP_EATING = 'Хватит есть! :/'
 
     def get_calories_remained(self):
         """Function for calculation your calories balance at this moment"""
-        dayStats = self.get_today_stats()
-        limit = self.limit
-        N = limit - dayStats
-        
-        if dayStats < limit:
-            return (f'Сегодня можно съесть еще что-нибудь,'
-                   f'но с общей каллорийностью не более {N} кКал :)')
-        else:
-            return 'Хватит есть! :/'
+        isDifference = self.getDifference()        
+        if isDifference > 0:
+            return self.CALORIES_BALANCE.format(balance=isDifference)
+        return self.STOP_EATING
 
 if __name__ == '__main__':
 
     cash = CashCalculator(500)
 
-    cash.add_record(Records(amount = 250,
+    cash.add_record(Records(amount = 500,
                            comment = 'кафе',
-                           date = '14.08.2022'))
+                           date = '22.08.2022'))
 
     cash.add_record(Records(amount = 50,
                            comment = 'продукты',
-                           date = '14.08.2022'))
+                           date = '20.08.2022'))
 
-    cash.add_record(Records(amount = 200,
+    cash.add_record(Records(amount = 250,
                            comment = 'йога',
-                           date = '14.08.2022'))
-
-    print(cash.get_today_cash_remained('USD'))
+                           date = '20.08.2022'))
+       
+    print(cash.get_today_cash_remained('USD_EURO'))
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Calculator
-CaloriesCalculator and CashCalculator
+There are two calculators in this module: CaloriesCalculator and
+CashCalculator.
+
+They allow us to record calories consumption/
+spending during the day
+
+CaloriesCalculator help us to control the amount of calories that you eat
+during the day. It calculate the rest of calories for achieving the day limit.
+
+CashCalculator summarizes spending and calculate rest of money that you can
+spend during day.
+
+
