62cd02d31491308f4d2d0e5472268373a365ea9e
Aksyon <aksenovaleksander94@gmail.com>
2022-08-26 21:36:34 +0500

    Readme file done

--- CalculatorLogic.py
+++ CalculatorLogic.py
@@ -8,7 +8,7 @@ DATE_FORMAT = '%d.%m.%Y'
 class Records():
     """Class for saving the records."""
 
-    def __init__(self, amount: float, comment: str, date: str) -> str:
+    def __init__(self, amount: float, comment: str, date: str = None) -> str:
         self.amount = amount
         self.comment = comment   
         if date is None:
@@ -24,23 +24,26 @@ class Calculator():
         self.limit = limit
         self.records = []
 
-    def add_record(self, record: str) -> str:
+    def add_record(self, record: str):
         """Function for addiing the records in list."""
         self.records.append(record)        
 
-    def get_today_stats(self) -> int:
+    def get_today_stats(self) -> float:
         """Function for getting the statistics for the whole this day."""
         today = dt.datetime.today().date()
-        return sum(record.amount for record in self.records if record.date == today)
+        return sum(record.amount for record in self.records if
+            record.date == today
+            )
 
-    def get_week_stats(self) -> int:
+    def get_week_stats(self) -> float:
         """Function for getting the statistics for previous week."""
         today = dt.datetime.today().date()
         day_week_ago = today - dt.timedelta(days=6)
         return sum(amount for amount in self.records if day_week_ago 
-                   <= amount.date < today)
+            <= amount.date < today
+            )
 
-    def get_difference(self) -> int:
+    def get_difference(self) -> float:
         return self.limit - self.get_today_stats()
     
 
@@ -51,29 +54,33 @@ class CashCalculator(Calculator):
                 'CNY' : (8.67, 'в рублях'),
                 'KZT' : (0.13, 'в рублях'), 
                 'RUB' : (1, 'в рублях')
-}
-    CASH_BALANCE_PLUS = 'На сегодня осталось {currency_name}: {balance}'
+        }
+    CASH_BALANCE_PLUS = 'На сегодня осталось {currency_name}: {balance} :)'
     CASH_BALANCE_MINUS = ('Денег нет, но вы держитесь. Долг {currency_name}: '
-                         '{balance}')
+                         '{balance} :(')
     CASH_BALANCE_ZERO = 'Денег нет, но вы держитесь :/'
 
     def get_today_cash_remained(self, currency: str = 'RUB') -> str:
         """Function for calculation of your cash balance at this moment."""               
         try:
-            get_rate = self.CURRENCY[currency]
+            currency_rate = self.CURRENCY[currency]
         except KeyError:
             raise KeyError(f'Вы ввели не поддерживаемую валюту: {currency}')
 
-        get_cash_remained = self.get_difference() * get_rate[0]
+        cash_remained = self.get_difference() * currency_rate[0]
         
         if self.get_difference() == 0:
             return self.CASH_BALANCE_ZERO
 
         if self.get_difference() > 0:
-            return self.CASH_BALANCE_PLUS.format(currency_name =
-                    get_rate[1], balance=get_cash_remained)
+            return self.CASH_BALANCE_PLUS.format(
+                currency_name=currency_rate[1],
+                balance=cash_remained
+                )
         return self.CASH_BALANCE_MINUS.format(
-            currency_name = get_rate[1], balance=get_cash_remained)
+            currency_name=currency_rate[1],
+            balance=cash_remained
+            )
 
         
 class CaloriesCalculator(Calculator):
@@ -92,9 +99,9 @@ if __name__ == '__main__':
 
     cash = CashCalculator(500)
 
-    cash.add_record(Records(amount = 650,
+    cash.add_record(Records(amount = 600,
                            comment = 'кафе',
-                           date = '25.08.2022'))
+                           date = '26.08.2022'))
 
     cash.add_record(Records(amount = 50,
                            comment = 'продукты',
--- README.md
+++ README.md
@@ -2,13 +2,40 @@
 There are two calculators in this module: CaloriesCalculator and
 CashCalculator.
 
-They allow us to record calories consumption/
-spending during the day
+CaloriesCalculator record calories consumption, control the amount
+of day income calories by calculating the rest of calories for
+achieving the day limit.
 
-CaloriesCalculator help us to control the amount of calories that you eat
-during the day. It calculate the rest of calories for achieving the day limit.
+CashCalculator record cash spending, summarize them and calculate
+rest of money that you can spend during day.
 
-CashCalculator summarizes spending and calculate rest of money that you can
-spend during day.
+Day and week statistics are available in both of calculators.
 
+Inheritance is the main OOP technolodgy that used in these modules.
 
+Copyright (C) <2022>  <Aleksandr Aksyonov>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+How to start program:
+1) Import module CalculatorLogic
+2) Create object by using <object name> = <calculator_name>(limit), 
+where calculator_name is CashCalculator or CaloriesCalculator
+3) Add some records by using:
+    <objecct name>.add_record(Records(amount = .., comment = '..', date = '..')),
+    if the record about today, it is allowed to skip the date.
+4) Available methods are:
+get_today_cash_remained('currency'),
+get_calories_remained,
+get_week_stats.
